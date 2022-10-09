353965524cbe260c63ecf45b9d5f6de72961d45e
Anh <84457154+anhyoungprofessional@users.noreply.github.com>
2022-10-09 16:25:37 -0400

    try out tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -25,24 +25,25 @@ function App() {
   );
 
   return (
-    <div className="container">
-      <div className="search-container">
-        <h2> Search</h2>
+    <div className="container mx-auto w-3/5">
+      <div className="search-container flex items-center justify-center m-12">
         <input
           onChange={(e) => setSearch(e.target.value)}
-          className="search-input"
+          className="border-indigo-500 active:border-indigo-700 border-solid border-2 rounded w-1/4 p-2"
           type="text"
+          placeholder="Search"
         />
       </div>
-      <div className="label">
-        <div className="label-left">
-          <p>Coin</p>
+      <div className="flex justify-between text-slate-500 mb-2">
+        <div className="left-label">
+          <p>Name</p>
         </div>
-        <div className="label-right">
+        <div className="right-label flex items-center justify-between w-4/5 child:w-1/5">
           <p>Price</p>
-          <p>Change(24h)</p>
-          <p>24h volume</p>
+          <p>24h Change</p>
+          <p>% Change</p>
           <p>Market Cap</p>
+          <p></p>
         </div>
       </div>
       {filterCoin.map((coin, index) => {
--- src/Coin.jsx
+++ src/Coin.jsx
@@ -2,33 +2,44 @@ import React from "react";
 
 function Coin(props) {
   const coin = props.coin;
-  const index = props.index;
+  const formatCash = (n) => {
+    if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(2) + "M";
+    if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(2) + "B";
+    if (n >= 1e12) return +(n / 1e12).toFixed(2) + "T";
+  };
+  const convertValue = (val) => {
+    return val.toLocaleString(undefined, { minimumFractionDigits: 2 });
+  };
   console.log(coin);
   return (
-    <div className="coin-container">
-      <div className="coin-container-left">
-        <div className="coin-img-name">
-          {" "}
-          <img className="coin-img" src={coin.image} alt="" />
-          <div className="coin-name">
-            <p>{coin.name}</p>
-            <p className="coin-symbol">{coin.symbol.toUpperCase()}</p>
-          </div>
+    <div className="coin-container flex justify-between items-center border-solid border-b py-2">
+      <div className="left-values flex gap-4 items-center">
+        {" "}
+        <img className="w-8 h-8" src={coin.image} alt="" />
+        <div className="coin-name">
+          <p>{coin.name}</p>
+          <p className="text-slate-500">{coin.symbol.toUpperCase()}</p>
         </div>
       </div>
-      <div className="coin-container-right">
-        <p className="coin-price">${coin.current_price}</p>
+      <div className="right-values flex items-center justify-between w-4/5 child:w-1/5">
+        <p className="coin-price ">${convertValue(coin.current_price)}</p>
+        <p>${convertValue(coin.price_change_24h)}</p>
         <p
           className={
             coin.price_change_percentage_24h > 0
-              ? "positive-change"
-              : "negative-change"
+              ? "text-green-500"
+              : "text-red-500"
           }
         >
           {coin.price_change_percentage_24h.toFixed(2)}%
         </p>
-        <p>{coin.total_volume}</p>
-        <p>${coin.market_cap}</p>
+        <p>${formatCash(coin.market_cap)}</p>
+        <p>
+          {" "}
+          <button className="bg-indigo-500 hover:bg-indigo-700 text-white py-2 px-4 rounded">
+            Trade
+          </button>
+        </p>
       </div>
     </div>
   );
