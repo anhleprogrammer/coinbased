3f0402c1006acbe89a1dfb649ee3b3cb9f5015ce
Anh <84457154+anhyoungprofessional@users.noreply.github.com>
2022-11-01 21:27:54 -0400

    fix nft collection fetch

--- src/components/CoinInfo.jsx
+++ src/components/CoinInfo.jsx
@@ -1,24 +1,24 @@
 import { React, useState, useEffect } from "react";
-import { exportedMethods } from "../utils/main.js";
+import { exportedMethods } from "../utils/alchemy.js";
 import axios from "axios";
 const url = exportedMethods.historicalChart2();
 
 function CoinInfo() {
-  const [historicalData, setHistoricalData] = useState();
-  useEffect(() => {
-    try {
-      async function getData() {
-        const coinData = await axios.get(url);
-        setHistoricalData(coinData.data);
-        return coinData;
-      }
-      getData();
-    } catch (err) {
-      console.log(err);
-    }
-  }, []);
+  // const [historicalData, setHistoricalData] = useState();
+  // useEffect(() => {
+  //   try {
+  //     async function getData() {
+  //       const coinData = await axios.get(url);
+  //       setHistoricalData(coinData.data);
+  //       return coinData;
+  //     }
+  //     getData();
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // }, []);
 
-  console.log(historicalData);
+  // console.log(historicalData);
 
   return <div>CoinInfo</div>;
 }
